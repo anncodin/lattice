55bbc6a4ad14a9355eeae63027d810697bc63427
AnnCodin <qantoinnette23@gmail.com>
2026-05-07 22:59:16 +0000

    Add menu icon

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/lattice-logo.png";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="navbar container">
+    <nav className="navbar container">
       <Link to="/">
         <img src={logo} alt="Logo" className="logo" />
       </Link>
-      <ul>
-        <NavLink to="/">
+      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+        <NavLink to="/" onClick={() => setMenuOpen(false)}>
           <li>Home</li>
         </NavLink>
-        <NavLink to="/about">
+        <NavLink to="/about" onClick={() => setMenuOpen(false)}>
           <li>About</li>
         </NavLink>
-        <NavLink to="/development">
+        <NavLink to="/development" onClick={() => setMenuOpen(false)}>
           <li>Development</li>
         </NavLink>
-        <NavLink to="/onsale">
+        <NavLink to="/onsale" onClick={() => setMenuOpen(false)}>
           <li>Onsale</li>
         </NavLink>
-        <NavLink to="/contact">
+        <NavLink to="/contact" onClick={() => setMenuOpen(false)}>
           <li>Contact</li>
         </NavLink>
+        <li>
+          {" "}
+          <button className="btn btn-2 mobile-btn">
+            Get In Touch <i class="ri-arrow-right-up-line"></i>
+          </button>
+        </li>
       </ul>
       <div className="navbar-btns">
         <button className="btn btn-1">
@@ -35,7 +42,14 @@ const Navbar = () => {
           Get In Touch <i class="ri-arrow-right-up-line"></i>
         </button>
       </div>
-    </div>
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? (
+          <i class="ri-close-fill"></i>
+        ) : (
+          <i class="ri-menu-4-line"></i>
+        )}
+      </div>
+    </nav>
   );
 };
 
