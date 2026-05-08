ed4eef94f5bcb7984397e55718710c11f13e664c
AnnCodin <qantoinnette23@gmail.com>
2026-05-08 12:03:03 +0000

    Add hero background and text

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="home">
+      <div className="hero container">
+        <div className="hero-text">
+          <p>potential of design</p>
+          <h1>Designing the Art of living</h1>
+          <p id='subtext'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
+            repudiandae.
+          </p>
+          <button className="btn hero-btn">
+            Take a Book <i class="ri-arrow-right-up-line"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
