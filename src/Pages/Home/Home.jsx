a38c587cc2620b62a6389326eca117709f50e0af
AnnCodin <qantoinnette23@gmail.com>
2026-05-08 12:33:28 +0000

    Add background overlay and button

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Home.css";
+import back_icon from "../../assets/back-icon.png";
+import next_icon from "../../assets/next-icon.png";
 
 const Home = () => {
   return (
     <div className="home">
       <div className="hero container">
+        <img src={back_icon} alt="" className="back-btn" />
+        <img src={next_icon} alt="" className="next-btn" />
         <div className="hero-text">
           <p>potential of design</p>
           <h1>Designing the Art of living</h1>
-          <p id='subtext'>
+          <p id="subtext">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
             repudiandae.
           </p>
